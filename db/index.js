e41a366d807fa0e12f02eef0cc8a761043c86faa
Ivan Ballester <irvan.baloncesto@gmail.com>
2024-08-13 10:01:56 +0200

    uri

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const MONGO_URI =
-  process.env.MONGODB_URLI ||
+  process.env.MONGODB_URI ||
   "mongodb://127.0.0.1:27017/OlimpoCinematografico-server";
 
 mongoose
