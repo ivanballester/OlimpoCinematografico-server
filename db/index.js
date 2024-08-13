f46e733d2c7c7455d8d1ab6480714950522f7681
Ivan Ballester <irvan.baloncesto@gmail.com>
2024-08-13 10:17:47 +0200

    cambio de nombre

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const MONGO_URI =
-  process.env.MONGODB_URI ||
-  "mongodb://127.0.0.1:27017/OlimpoCinematografico-server";
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/TrueFilms-db";
 
 mongoose
   .connect(MONGO_URI)
